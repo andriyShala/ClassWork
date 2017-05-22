776d559da37a4ea73188921261ca4e2b26b91f57
andriyShala <andriy.shala@i.ua>
2017-05-22 11:52:16 +0300

    1

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 140px">
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:DropDownList ID="DropDownList3" runat="server">
+        </asp:DropDownList>
+        <asp:TextBox ID="TextBox1" runat="server" Width="101px"></asp:TextBox>
+        <asp:DropDownList ID="DropDownList2" runat="server">
+        </asp:DropDownList>
+        <asp:TextBox ID="TextBox2" runat="server" Height="16px" ReadOnly="True" style="margin-top: 1px"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Convert" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DataSet dataSet = new DataSet();
+                dataSet.ReadXml(Server.MapPath("course.xml"));
+                foreach (var items in dataSet.Tables[0].Columns)
+                {
+                    DropDownList2.Items.Add(items.ToString());
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml(Server.MapPath("course.xml"));
+            double number = 0;
+            try
+            {
+
+                number = Convert.ToDouble(TextBox1.Text);
+            }
+            catch 
+            {
+                
+            }
+
+            TextBox2.Text = (number/
+                            Convert.ToDouble(
+                                dataSet.Tables[0].Rows[0][DropDownList2.SelectedIndex].ToString().Replace(" ", ""))).ToString();
+
+        }
+    }
+}
